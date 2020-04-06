0ff29ff727e1b85d0ae730cfa307eca0922cebee
goski89 <dom.gorski@gmail.com>
2020-04-07 01:28:38 +0200

    conftest oraz generowanie reportu allure

--- travel_sel_2/page_object_pattern/pages/search_hotel.py
+++ travel_sel_2/page_object_pattern/pages/search_hotel.py
@@ -1,11 +1,11 @@
 from ..locators.locators import SearchHotelLocators as Locator
 import logging
+import allure
 
 class SearchHotelPage:
     def __init__(self, driver):
         self.driver = driver
         self.logger = logging.getLogger(__name__)
-        self.website = "http://www.kurs-selenium.pl/demo"
 
         self.search_hotel_span_xpath = "//span[text()='Search by Hotel or City Name']"
         self.search_hotel_input_xpath = "//div[@id='select2-drop']//div//input"
@@ -23,6 +23,7 @@ class SearchHotelPage:
 
         self.search_button_xpath = "//button[text()=' Search']"
 
+    @allure.step("Setting city name to Dubai {1}")
     def set_city(self, city):
         self.logger.info(f"Setting city {city}")
         self.driver.find_element_by_xpath(self.search_hotel_span_xpath).click()
@@ -32,7 +33,9 @@ class SearchHotelPage:
         search_input_obj.send_keys(city)
 
         self.driver.find_element_by_xpath(self.location_match_span_xpath(city)).click()
+        allure.attach(self.driver.get_screenshot_as_png(), name="set_city", attachment_type=allure.attachment_type.PNG)
 
+    @allure.step(f"Set check_{'in' if {4} else 'out'}_date: {3}/{2}/{1}")
     def set_date_range(self, year, month, day, click=False):
         self.logger.info(f'Ustaw check_{"in" if click else "out"}_date: {day}/{month}/{year}')
         if click:
@@ -44,7 +47,10 @@ class SearchHotelPage:
         while current_month_obj.text != temp:
             next_btn_obj.click()
         self.driver.find_element_by_xpath(self.day_td_xpath(str(day))).click()
+        allure.attach(self.driver.get_screenshot_as_png(), name=f"set_date_check_{'in' if click else 'out'}", attachment_type=allure.attachment_type.PNG)
 
+
+    @allure.step("Set Adult: {1} and Childrens: {2}")
     def set_travellers(self, adult, child):
         self.logger.info(f"Adults: {adult}, child: {child}")
         self.driver.find_element_by_xpath(self.traveller_input_xpath).click()
@@ -59,6 +65,8 @@ class SearchHotelPage:
         child_input_obj.click()
         child_input_obj.clear()
         child_input_obj.send_keys(str(child))
+        allure.attach(self.driver.get_screenshot_as_png(), name="set_travellers", attachment_type=allure.attachment_type.PNG)
 
+    @allure.step("Click Search")
     def search(self):
         self.driver.find_element_by_xpath(self.search_button_xpath).click()
--- travel_sel_2/page_object_pattern/pages/search_results.py
+++ travel_sel_2/page_object_pattern/pages/search_results.py
@@ -1,5 +1,7 @@
 from ..locators.locators import SearchResultsLocators as Locator
 import logging
+import allure
+
 
 class SearchResultPage:
 
@@ -7,14 +9,16 @@ class SearchResultPage:
         self.driver = driver
         self.logger = logging.getLogger(__name__)
 
+    @allure.step("Show list hotel")
     def get_hotel_names(self):
-
         hotels_list = self.driver.find_elements_by_xpath(Locator.hotel_names_b_xpath)
         hotel_names = [hotel.get_attribute('textContent') for hotel in hotels_list]
         for hotel in hotel_names:
             self.logger.info(f"Nazwa hotelu: {hotel}")
+        allure.attach(self.driver.get_screenshot_as_png(), name="show_hotels_list", attachment_type=allure.attachment_type.PNG)
         return hotel_names
 
+    @allure.step("Show hotel prices")
     def get_hotel_prices(self):
         prices_list = self.driver.find_elements_by_xpath(Locator.hotel_prices_b_xpath)
         hotel_prices = [price.get_attribute('textContent') for price in prices_list]
